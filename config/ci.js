25b2f179523a89bcef7963457c18d7e4f4de7af1
Zhe Wang <wangzhe.dut@gmail.com>
2018-08-18 01:12:54 -0700

    ff

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID:
+      '151362285445-1hquu5ni1d7pmdjg57vjarmi0dglh7h7.apps.googleusercontent.com',
+    googleClientSecret: 'uod72IaRqvYXi0mbt2bXCkg1',
+    mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+    cookieKey: 'inioioipqkodnncjmcmcxzweqybgfnsjdaijwqjdwq',
+    redisUrl: 'redis://127.0.0.1:6379',
+};
--- config/dev.js
+++ config/dev.js
@@ -3,5 +3,6 @@ module.exports = {
     '151362285445-1hquu5ni1d7pmdjg57vjarmi0dglh7h7.apps.googleusercontent.com',
   googleClientSecret: 'uod72IaRqvYXi0mbt2bXCkg1',
   mongoURI: 'mongodb://zhedev:zhedev@ds113626.mlab.com:13626/zhe-emaily-dev',
-  cookieKey: 'inioioipqkodnncjmcmcxzweqybgfnsjdaijwqjdwq'
+  cookieKey: 'inioioipqkodnncjmcmcxzweqybgfnsjdaijwqjdwq',
+  redisUrl: 'redis://127.0.0.1:6379',
 };
